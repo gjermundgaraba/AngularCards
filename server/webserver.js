0c8d3b8b78d331dae2c16f1aa86e9dbc9e95e1b2
Gjermund Bjaanes <bjaanes@gmail.com>
2015-03-11 20:46:33 +0100

    Added mongodb database connection with mongojs.

--- server/webserver.js
+++ server/webserver.js
@@ -2,24 +2,32 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+var databaseUrl = "mydb";
+var collections = ["cards"];
+var db = require("mongojs").connect(databaseUrl, collections);
+
 app.use(bodyParser.json());
 app.use(express.static('app'));
 
-var cards = [
-    {
-        "title": "This is a title",
-        "body": "This is a body",
-        "createdAt": +new Date()
-    }
-];
-
 app.get('/cards', function(request, response) {
-    response.json(cards);
+    db.cards.find(function(err, cards) {
+        if( err ) {
+            console.log("Cards not found");
+        }
+        else {
+            console.log("Cards found");
+            response.json(cards);
+        }
+
+    });
 });
 
 app.post('/cards', function(request, response) {
-    cards.push(request.body);
-    response.json(request.body);
+    db.cards.save(request.body, function(err, saved) {
+        if( err || !saved ) console.log("Card not saved");
+        else console.log("Card saved");
+        response.end();
+    });
 });
 
 app.listen(3000, function() {
